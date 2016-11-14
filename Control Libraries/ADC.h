c9e1e473abc5bf9fd41dc268e46d35b606036ddc
Cameron Handeland <Cameron Handeland>
2016-11-14 10:19:53 -0700

    Added prototype functions for ADC library and introduced timer control librariesl.

---
+++ Control Libraries/ADC.h
@@ -0,0 +1,8 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+void setupADC(uint8_t port, uint8_t pin); //set the necessary registers for a pin to read analog voltages.
+uint16_t readADC(); //begin a read. The ADC handler will send the value over UART once it's ready
+
+
+#endif
